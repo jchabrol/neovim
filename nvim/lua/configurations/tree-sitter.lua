c4d2233a3d7dee742771126988e45d0caa4912da
Jerome Chabrol <jchabrol@wallix.com>
2026-05-13 17:22:04 +0200

    Update nvim-treesitter to main branch.
    This fix the issue on robot framework syntax color.

--- nvim/lua/configurations/tree-sitter.lua
+++ nvim/lua/configurations/tree-sitter.lua
@@ -1,26 +1,10 @@
--- treesitter configuration
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = {"robot", "typescript", "javascript","jsonc" },
+local ensure_installed = { "robot", "typescript", "javascript", "jsonc" }
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+require("nvim-treesitter").install(ensure_installed)
 
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = false,
-
-
-  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
-  highlight = {
-    enable = true,
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
-}
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = ensure_installed,
+  callback = function()
+    pcall(vim.treesitter.start)
+  end,
+})
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,9 +1,10 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
     build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
-    cmd = { "TSUpdate", "TSInstall", "TSInstallInfo", "TSUpdateSync" },
+    cmd = { "TSUpdate", "TSInstall", "TSInstallInfo" },
     config = function()
       require("configurations.tree-sitter")
     end,
